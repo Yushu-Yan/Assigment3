347cabe40dfe7fdbaf4575fd83b85d70e3d29fb1
Yushu-Yan <60760112+Yushu-Yan@users.noreply.github.com>
2020-04-09 18:40:17 -0400

    Update my_answers.py

--- src/mypkg/my_answers.py
+++ src/mypkg/my_answers.py
@@ -13,7 +13,7 @@ import numpy as np
 #### new_arr:	the new reformed array as the output
 #### reform the array to a new array with size(nRow,nCol)
 def reform_array_dimension_col_wise(arr, nRow, nCol):
-	new_arr =  					# write your code here
+	new_arr = np.reshape(arr,(nRow, nCol))		# write your code here
 	return new_arr
 
 
@@ -21,7 +21,7 @@ def reform_array_dimension_col_wise(arr, nRow, nCol):
 #### new_arr:	the new generated array as the output
 #### stack the column summation below the bottom of the array
 def append_sum_of_array(arr):
-	new_arr =  					# write your code here
+	new_arr = np.vstack((arr, np.sum(arr,axis=0)))	# write your code here
 	return new_arr 
 
 
@@ -29,15 +29,15 @@ def append_sum_of_array(arr):
 #### new_arr:	the new generated array as the output
 #### delete the top row and ending column from the array
 def remove_topRow_endCol_from_array(arr):
-	new_arr = 				# write your code here
+	new_arr = np.delete(np.delete(arr,0,axis=0),-1,axis=1)	# write your code here
 	return new_arr
 
 #### arr: 		the input array
 #### new_arr:	the new generated array as the output
 #### calculate the product of each row and append to the array, use row_product to save the product value and add to the new array
 def add_row_product_to_array(arr):
-	row_product = 				# write your code here
-	new_arr =  				# write your code here
+	row_product = np.prod(arr,axis=1)	# write your code here
+	new_arr = np.hstack((arr,row_product.reshape(3,1)))   # write your code here
 	return new_arr
 
 
